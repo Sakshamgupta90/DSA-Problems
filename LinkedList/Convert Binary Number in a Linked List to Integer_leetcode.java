9d430a1d8651e35269bf5a22f40dbe84b6fb2c4f
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2022-06-16 20:12:15 +0530

    Update Convert Binary Number in a Linked List to Integer_leetcode.java

--- LinkedList/Convert Binary Number in a Linked List to Integer_leetcode.java
+++ LinkedList/Convert Binary Number in a Linked List to Integer_leetcode.java
@@ -1,1 +1,15 @@
-// Method: we will iterate through our linkedlist and side by side converting the number into binary number by multiplying the value into 2 and suming them up;
+// Method: we will iterate through our linkedlist and side by side converting the number into binary number by multiplying the value into 2 and suming them up.
+
+//code:
+public int getDecimalValue(ListNode head) {
+        ListNode curr = head;
+        int sum = 0;
+        while(curr != null){
+            sum = (2*sum) + curr.val;
+            curr = curr.next;
+        }
+        return sum;
+    }
+
+//Time complexity: O(N)
+//Space complexity : O(1)
