93acdb64b9e97edf523cf9f24bd085fa35db4cf7
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2022-06-18 17:08:03 +0530

    create

---
+++ LinkedList/Multiply two LinkedLists_gfg.java
@@ -0,0 +1,22 @@
+//Method: we will first convert our linked list into number and then we will multiply our two numbers and return it.
+
+//code:
+public long multiplyTwoLists(Node l1,Node l2){
+          //add code here.
+         long num1 = 0, num2 = 0, mod = (long)1e9+7;
+        
+        while(l1 != null){
+            num1 = ((num1 * 10) + l1.data)%mod;
+            l1 = l1.next;
+        }
+        
+        while(l2 != null){
+            num2 = ((num2 * 10) + l2.data)%mod;
+            l2 = l2.next;
+        }
+        
+        return (num1*num2)%mod;
+   }
+
+//Time complexity: O(N)
+//Space complexity: O(1)
